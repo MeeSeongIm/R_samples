a28711b06a06112c1281c546851bd2741c77f6ae
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-10-31 21:51:53 -0500

    Create ui.R

---
+++ fns_increments/ui.R
@@ -0,0 +1,25 @@
+# slider and histogram 
+ 
+ 
+shinyUI(pageWithSidebar(
+  headerPanel("Functions and Increments."),
+  sidebarPanel(
+    textInput(inputId="text1", label = "First Input:"),
+    textInput(inputId="text2", label = "Second Input:")
+    ),
+  mainPanel(
+    p("a = Your First Input"),
+    textOutput("text1"),
+    p("b = Your Second Input"),
+    textOutput("text2"),
+    p("The sum of 'a' squared and 'b' squared:"),
+    textOutput("text3"),
+    p("Each time a person reloads the page, this increment goes up by one."),
+    textOutput("text4"),
+    p("Inside text, but non-reactive"),
+    textOutput("text5")
+    )
+  ))
+  
+  
+  
